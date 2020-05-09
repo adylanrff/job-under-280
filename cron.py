e87459172919143dbc16effe59cabe74782317f5
Adylan Roaffa <adylan.dev@gmail.com>
2020-05-10 00:46:02 +0700

    cron: Add working directory

--- cron.py
+++ cron.py
@@ -1,6 +1,8 @@
-from crontab import CronTab
+import os
 import getpass
+from crontab import CronTab
 
+cwd = os.getcwd()
 user = getpass.getuser()
 print("Setting cron for user: {}".format(user))
 
@@ -9,11 +11,11 @@ cron = CronTab(user=user)
 cron.remove_all()
 
 # Set tweet job every 30 minutes
-tweet_job = cron.new("python3 jobs-under-280/core.py --tweet", comment="Tweet Jobs")
+tweet_job = cron.new("python3 {}/jobs-under-280/core.py --tweet".format(cwd), comment="Tweet Jobs")
 tweet_job.minute.every(15)
 
 # Set scrap jobs daily
-scrap_job = cron.new("python3 jobs-under-280/core.py --scrap_job", comment="Scrap jobs from HN & AngelList")
+scrap_job = cron.new("python3 {}/jobs-under-280/core.py --scrap_job".format(cwd), comment="Scrap jobs from HN & AngelList")
 scrap_job.hour.on(13)  
 scrap_job.hour.also.on(1) # Set to * 2,*/10 * * *
 
