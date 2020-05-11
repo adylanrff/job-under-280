ac514a13059c5f6f79f1aecd87dbb3a0104a6c86
Adylan Roaffa <adylan.dev@gmail.com>
2020-05-11 12:59:27 +0700

    modify cron for python path

--- cron.py
+++ cron.py
@@ -11,13 +11,13 @@ cron = CronTab(user=user)
 cron.remove_all()
 
 # Set tweet job every 30 minutes
-tweet_job = cron.new("python3 {}/jobs-under-280/core.py --tweet".format(cwd), comment="Tweet Jobs")
+tweet_job = cron.new("python3 {}/jobs_under_280/core.py --tweet".format(cwd), comment="Tweet Jobs")
 tweet_job.hour.during(3, 8)
 tweet_job.hour.also.during(11, 15)
 tweet_job.minute.also.on(0)
 
 # Set scrap jobs daily
-scrap_job = cron.new("python3 {}/jobs-under-280/core.py --scrap_job".format(cwd), comment="Scrap jobs from HN & AngelList")
+scrap_job = cron.new("python3 {}/jobs_under_280/core.py --scrap_job".format(cwd), comment="Scrap jobs from HN & AngelList")
 scrap_job.every(12).hours() # set to every 12 hours  
 
 cron.write()
